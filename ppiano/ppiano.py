184976d663618cabc1851958cb9af04894e5e759
RykerZhangYujie <32808589+RykerZhangYujie@users.noreply.github.com>
2018-01-21 09:16:51 +0800

    mk

--- ppiano/ppiano.py
+++ ppiano/ppiano.py
@@ -20,8 +20,6 @@ for p in ports:
 #song1 = ['star','1','1','5','5','6','6','5','5','4','4','3','3','2','2','1','1']
 #song2 = ['hallo','1','2','3','1','1','2','3','1','3','4','5','3','4','5']
 
-
-
 f = open('mysongs.csv', 'r')
 data = f.read()
 rows = data.split('\n')
@@ -40,19 +38,29 @@ album["tinkelstar"]=0
 #songs_dictionary={'tinklestar':1,'dadaotuhao':2,'RadetzkyMarsch':3,'xjbsong':4,'clash royale':5}
 songs_dictionary=get_song_dictionary(songs)
 
-song_dic={'tinkelstar':1,'dadaotuhao':2,'RadetzkyMarsch':3,'RadetzkyMarsch2':4,'xjbsong':5,'clash royale':6}
-#ser=serial.Serial(port='COM4')
-#ser=serial.Serial(port]='/dev/ttymodem542')
-#ifha;oifhad;oifh
 def run():
+
+mysongs_data=[]
+f=open("mysongs.csv","r")
+data =  f.read()
+rows = data.split('\n')
+for row in rows:
+    split_row = row.split(",")
+    mysongs_data.append(split_row)
+for row in songs_data:
+    mysongs.append(row[1])
+count = 0
+for item in  mysongs:
+    count = count + 1
+new_mysongs = songs[1:6]
 
     action = "empty"
     while action != "q":
         print ('select 1.input song sequence, number,select 2 , input song namen , q and others for quit')
         action = input("> ")
         if action=='1' :
-            print ('select in which song do you want to play:for example：1,2,3,4,5, q and others for quit')
-            song_number = int(input("> "))
+        print ('select in which song do you want to play:for example：1,2,3,4,5, q and others for quit')
+        song_number = int(input("> "))
             print("song number is:")
             print(song_number)
             for notes in songs[song_number]:
@@ -62,8 +70,14 @@ def run():
         elif action == "2":
             print ('select in which song do you want to play:tinklestar,dadaotuhao,RadetzkyMarsch,xjbsong,clash royale,q and others for quit')
             song_name = input("> ")
+            import pandas as pd
+            mysongs=pd.read_csv('mysongs.csv')
+            import numpy as np
+            print ('select in which song do you want to play:for example：1,2,3,4,5, q and others for quit')
+            song_number = int(input("> "))
+            songs_name_row = mysongs.loc[song_number]
             print("songs name is:")
-            print(song_name)
+            print(songs_name_row)
             song_number=songs_dictionary[song_name]
             print("song number is:")
             print(song_number)
@@ -73,3 +87,5 @@ def run():
                 time.sleep(1)
         else :
             return
+
+run()
